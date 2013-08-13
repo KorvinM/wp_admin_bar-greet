ba0dfc3b3d9f284fc00aabff9b1d9d3a58c232ad
Korv <korvin@email.fake>
2013-08-13 18:36:21 +0100

    Remove if option is not empty condition to master branch

--- ydhw.php
+++ ydhw.php
@@ -75,15 +75,12 @@ if(class_exists('kvn_ydwh'))
     // replace WordPress Howdy in WordPress 3.3, see http://wp-snippets.com/replace-howdy-in-wordpress-3-3-admin-bar/
 	function kvn_replace_howdy( $wp_admin_bar ) {
 		$option = get_option('greeting');
-		if ($option !=''){//if option is empty
-			$my_account=$wp_admin_bar->get_node('my-account');
-			$newtitle = str_replace( 'Howdy,', $option, $my_account->title );            
-			$wp_admin_bar->add_node( array(
-				'id' => 'my-account',
-				'title' => $newtitle,
-			) );
-		}
-			
+		$my_account=$wp_admin_bar->get_node('my-account');
+		$newtitle = str_replace( 'Howdy,', $option, $my_account->title );            
+		$wp_admin_bar->add_node( array(
+			'id' => 'my-account',
+			'title' => $newtitle,
+		) );
 	}
 	
 	add_filter( 'admin_bar_menu', 'kvn_replace_howdy');
