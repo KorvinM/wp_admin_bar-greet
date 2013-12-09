ec7b25ded9733f9c2b35066fa5e604d4bffbaeed
Korv <korvin@email.fake>
2013-12-09 15:30:59 +0000

    take main function out of contructor function and put it back where it was before. Tidy up formatting & indentation

--- wpadminbar-greet.php
+++ wpadminbar-greet.php
@@ -39,28 +39,13 @@ if(!class_exists('kvn_ydwh'))
             require_once(sprintf("%s/settings.php", dirname(__FILE__)));
             $kvn_ydwh_Settings = new kvn_ydwh_Settings();
 			
-			/* replace WordPress Howdy in WordPress 3.3
-		   see http://wp-snippets.com/replace-howdy-in-wordpress-3-3-admin-bar/ */
-		
-			function kvn_replace_ydwh( $wp_admin_bar ) {
-
-				$ydwh_option = get_option('greeting');
-				if ($ydwh_option != ''){//if option is not empty
-					$ydwh_my_account=$wp_admin_bar->get_node('my-account');
-					$ydwh_newtitle = str_replace( 'Howdy,', $ydwh_option, $ydwh_my_account->title );            
-					$wp_admin_bar->add_node( array(
-					'id' => 'my-account',
-					'title' => $ydwh_newtitle,
-					) );	
-				}
-			}
+			
 	
 			add_filter( 'admin_bar_menu', 'kvn_replace_ydwh');
         	
 		} // END public function __construct
 
-	   
-		
+	   		
 		/**
 		 * Activate the plugin
 		 */
@@ -89,8 +74,24 @@ if(class_exists('kvn_ydwh'))
 	$kvn_ydwh = new kvn_ydwh();
 	
     if(isset($kvn_ydwh)) {
-
-        // Add the settings link to the plugins page
+		
+		/* replace WordPress Howdy in WordPress 3.3
+	 	*  see http://wp-snippets.com/replace-howdy-in-wordpress-3-3-admin-bar/ 
+		*/
+		
+		function kvn_replace_ydwh( $wp_admin_bar ) {
+			$ydwh_option = get_option('greeting');
+			if ($ydwh_option != ''){//if option is not empty
+				$ydwh_my_account=$wp_admin_bar->get_node('my-account');
+				$ydwh_newtitle = str_replace( 'Howdy,', $ydwh_option, $ydwh_my_account->title );            
+				$wp_admin_bar->add_node( array(
+					'id' => 'my-account',
+					'title' => $ydwh_newtitle,
+					) );	
+			}
+		}
+        
+		// Add the settings link to the plugins page
         function kvn_ydwh_settings_link($links){
             $kvn_ydwh_settings_link = '<a href="options-general.php#ydwh_greet">Setting</a>';
             array_unshift($links, $kvn_ydwh_settings_link);
