2307f041f759112bc8b2f17841c4d5e1d1cd4de8
Korv <kjmobberley@gmail.com>
2014-08-09 01:40:54 +0100

    rm whitespace

--- settings.php
+++ settings.php
@@ -23,23 +23,23 @@ if(!class_exists('kvn_ydwh_Settings'))
 				'general',
 				'kvn_ydwh-section',
 				array(
-                    'field' => 'greeting'
-                )
-            );
+					'field' => 'greeting'
+				)
+			);
 		}// END public function admin_init
         
         public function settings_section_kvn_ydwh() {// callback to add title and help text
-		  echo '<h3 id="ydwh_greet">Toolbar Greeting</h3><em>Change the greeting in the toolbar. Default: <strong>Howdy</strong> </em><br>'; 
+		  echo '<h3 id="ydwh_greet">Toolbar Greeting</h3><em>Change the greeting in the toolbar. Default: <strong>Howdy</strong> </em><br>';
 		}
-		
+
 		public function settings_field_input_text($args) {//provides text inputs for settings fields
-            // Get the field name from the $args array
-            $field = $args['field'];
-            // Get the value of this setting
-            $value = get_option($field);
-            // echo a proper input type="text"
-            echo sprintf('<input type="text" name="%s" id="%s" value="%s" />', $field, $field, $value);
+			// Get the field name from the $args array
+			$field = $args['field'];
+			// Get the value of this setting
+			$value = get_option($field);
+			// echo a proper input type="text"
+			echo sprintf('<input type="text" name="%s" id="%s" value="%s" />', $field, $field, $value);
 		} // END public function settings_field_input_text($args)
-        
+
 	} // END class kvn_ydwh_Settings
 } // END if(!class_exists('kvn_ydwh_Settings'))
